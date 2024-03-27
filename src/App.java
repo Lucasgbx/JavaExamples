c810fb039422c14eb609133fc87b6b6cf91d0ed8
Lucas Brand <lucas.brand@premfina.com>
2024-03-27 12:57:30 +0000

    Added TrackedParcel examples

--- src/App.java
+++ src/App.java
@@ -4,7 +4,6 @@ public class App {
         System.out.println(p1.toString());
         System.out.println(p1.getDateSent().length());
 
-        	
         System.out.println(Parcel.getOrderedSides(20,50,30));
         System.out.println(Parcel.getOrderedSides(20,30,30));
         System.out.println(Parcel.getOrderedSides(30,30,30));
@@ -30,5 +29,56 @@ public class App {
 
         Parcel p16 = new Parcel("1 New Street", 4.3, 30, 35, 40);
         System.out.println(p16.createQuote(126));
+
+        TrackedParcel p9 = new TrackedParcel("1 New Street", 2.0, 30, 35, 40, 250);
+        System.out.println(p9.toString());
+        System.out.println("date sent: " + p9.getDateSent());
+        p9.addItem("Clock");
+        p9.addItem("Picture");
+        System.out.println(p9.toString());
+
+        TrackedParcel p10 = new TrackedParcel("1 New Street", 2.0, 30, 35, 40, 250);
+        p10.setDateAndTracking("0123456");
+        String months[] = {"Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"};
+        List<String> monthList = Arrays.asList(months);
+        String theYear = p10.getDateSent().substring(3, 6);
+        System.out.println(monthList.contains(theYear));
+        int theDay = Integer.parseInt(p10.getDateSent().substring(0,2));
+        System.out.println(0 < theDay && theDay < 32);
+        System.out.println(p10.getTrackingNumber().equals("0123456"));
+        System.out.println(p10.getDateSent().charAt(2) == ' ' && p10.getDateSent().charAt(6) == ' ');
+
+        TrackedParcel p11 = new TrackedParcel("1 New Street", 2.0, 30, 35, 40, 250);
+        p11.setDateAndTracking("0123456");
+        p11.setDateSent("01 Mar 2024");
+        System.out.println(p11.toString());
+        p11.addItem("Lion");
+        p11.addItem("Elephant");
+        System.out.println(p11.toString());
+
+        TrackedParcel p12 = new TrackedParcel("1 New Street", 2.0, 30, 35, 40, 250);
+        p12.setDateAndTracking("0123456");
+        p12.setDateSent("01 Mar 2024");
+        TrackedParcel p13 = new TrackedParcel("1 New Street", 2.0, 30, 35, 40, 250);
+
+        p13.setDateAndTracking("0123456");
+        p13.setDateSent("01 Mar 2024");
+        System.out.println(p12.equals(p13));
+        p13.setDateSent("");
+        System.out.println(p12.equals(p13));
+        p12.setDateAndTracking("0123458");
+        p12.setDateSent("");
+        System.out.println(p12.equals(p13));
+
+        Parcel p14 = new Parcel("1 New Street", 2.0, 30, 35, 40);
+        System.out.println(p13.equals(p14));
+
+        TrackedParcel p15 = new TrackedParcel("1 New Street", 2.0, 30, 35, 40, 250);
+        p15.setDateAndTracking("0123456");
+        p15.setDateSent("01May2024");
+        System.out.println(p15.hashCode());
+
+        TrackedParcel p17 = new TrackedParcel("1 New Street", 2.0, 30, 35, 40, 250);
+        System.out.println(p17.createQuote(126));
     }
 }
